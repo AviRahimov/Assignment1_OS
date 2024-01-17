13b6c1c8bafe305632acaacb53252cd392d95bc5
avi <rahimovavi3@gmail.com>
2024-01-17 15:50:01 +0200

    create tasks

---
+++ task1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int inf_rec(int num){
+    return inf_rec(num - 1);
+}
+
+
+int main(){
+    inf_rec(100);
+    return 0;
+}
---
+++ task2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    int num1, num2, num3;
+    // Ask the user to type 3 numbers
+    printf("Enter the length of the first side of the triangle: \n");
+    // Get and save the number the user types
+    scanf("%d", &num1);
+    printf("Enter the length of the second side of the triangle: \n");
+    scanf("%d", &num2);
+    printf("Enter the hypotenuse length: \n");
+    scanf("%d", &num3);
+
+    if((pow(num1, 2) + pow(num2, 2)) == (pow(num3, 2))){
+        double alpha = asin(num1/num3); 
+        double beta = asin(num2/num3);
+        double gama = asin(num3/num3);
+        printf("The size of the angles in your triangle are:%f, %f, %f \n", 
+        alpha, beta, gama);
+    }
+    else {
+        perror("Your triangle lengths not following pythagoras theorem\n");
+        exit(-1);
+    }
+    return 0;
+}
